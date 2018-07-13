3e0c046799677ca24cd45ddbcb818aeaf9be1820
WuTao88 <wutao.88@qq.com>
2018-07-13 11:16:01 +0800

    Initial commit

---
+++ src/main/java/com/server/cloud/server_B/ServerBApplication.java
@@ -0,0 +1,14 @@
+package com.server.cloud.server_B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBApplication.class, args);
+	}
+}
